d83fc621812c01fa56724409a47d33d238b8c0f8
Tonkx <Tonkx@TONKXY>
2022-10-27 13:21:36 -0600

    Add project files.

---
+++ Idea Pending_SMART/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Idea_Pending_SMART.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
