75596329fcc9434569a21e419eb464c2e3679b45
ctonks2 <45181762+ctonks2@users.noreply.github.com>
2022-11-06 13:12:50 -0700

    Update Attendance.cs

--- Idea Pending_SMART/Models/Attendance.cs
+++ Idea Pending_SMART/Models/Attendance.cs
@@ -19,19 +19,8 @@ namespace Idea_Pending_SMART.Models
         public char? MealEaten { get; set; }
 
         [Required]
-<<<<<<< Updated upstream
-        [ForeignKey("Class")]
-        public int? ClassID { get; set; }
-
-        [Required]
-        [ForeignKey("Student")]
-        public int StudentID { get; set; }
-=======
         [ForeignKey("Enrollment")]
         public int? EnrollmentID { get; set; }
-
->>>>>>> Stashed changes
-
 
     }
 
