ada206cc2a20c4cce0885db6b6288dc4c6d442c8
BenHo1937 <89957466+BenHo1937@users.noreply.github.com>
2022-11-15 15:41:27 -0700

    Update ApplicationDbContext.cs

--- Idea Pending_SMART/Data/ApplicationDbContext.cs
+++ Idea Pending_SMART/Data/ApplicationDbContext.cs
@@ -9,18 +9,33 @@ namespace Idea_Pending_SMART.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {}
+        public DbSet<School> School { get; set; }
 
         public DbSet<Semester> Semester { get; set; }
         public DbSet<Student> Students { get; set; }//student
         public DbSet<Class> Class { get; set; }
-        public DbSet<Application> Applications { get; set; }//Applications
 
-        public DbSet<ClassTime> ClassTimes => Set<ClassTime>();
+        public DbSet<Enrollment> Enrollments { get; set; }//enrollment
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Sponsor> Sponsor { get; set; }
+        public DbSet<ClassTime> ClassTimes { get; set; }
+        public DbSet<Applicant> Applicant { get; set; }
 
-        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+        public DbSet<Attendance> Attendance { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Permissions> Permissions { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<StudentNote> StudentNote { get; set; }
 
-        public DbSet<Student> AddStudents => Set<Student>();
+     /*  Chantel may need this later...
+      *  protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Enrollment>().HasKey(sc => new { sc.StudentID, sc.ClassID });
+            base.OnModelCreating(builder);
 
+
+        }*/
 
     }
 }
