a3367ea586f8ff0f07b92d49c380258a3ecc0172
BenHo1937 <phuc.gp7398@gmail.com>
2022-11-15 15:49:49 -0700

    ben commit

--- Idea Pending_SMART/Data/ApplicationDbContext.cs
+++ Idea Pending_SMART/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Idea_Pending_SMART.Dev_Chantel.Models;
+﻿using Idea_Pending_SMART.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,19 +8,34 @@ namespace Idea_Pending_SMART.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        {}
+        { }
+        public DbSet<School> School { get; set; }
 
         public DbSet<Semester> Semester { get; set; }
         public DbSet<Student> Students { get; set; }//student
         public DbSet<Class> Class { get; set; }
-        public DbSet<Application> Applications { get; set; }//Applications
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }//enrollment
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Sponsor> Sponsor { get; set; }
+        public DbSet<ClassTime> ClassTimes { get; set; }
+        public DbSet<Applicant> Applicant { get; set; }
 
-        public DbSet<ClassTime> ClassTimes => Set<ClassTime>();
+        public DbSet<Attendance> Attendance { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Permissions> Permissions { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<StudentNote> StudentNote { get; set; }
 
-        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+        /*  Chantel may need this later...
+         *  protected override void OnModelCreating(ModelBuilder builder)
+           {
+               builder.Entity<Enrollment>().HasKey(sc => new { sc.StudentID, sc.ClassID });
+               base.OnModelCreating(builder);
 
-        public DbSet<Student> AddStudents => Set<Student>();
 
+           }*/
 
     }
 }
