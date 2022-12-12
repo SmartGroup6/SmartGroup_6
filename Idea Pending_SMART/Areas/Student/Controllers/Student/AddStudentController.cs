12a76900cbc3fc38e17c20699dffabaf52925222
Travis Cassity <traviscassity@mail.weber.edu>
2022-12-12 05:25:00 -0700

    dfas

---
+++ Idea Pending_SMART/Areas/Student/Controllers/Student/AddStudentController.cs
@@ -0,0 +1,25 @@
+﻿using Idea_Pending_SMART.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Idea_Pending_SMART.Areas.Student.Controllers.Student
+{
+    //This should probably be an AddStudent method in the Student controller.
+    public class AddStudentController
+    {
+        /*
+            +checkCoursePrereq(CourseID)
+            +checkStudentAvailable(StudentID)
+            +checkClassSpace(ClassID)
+            +canStudentBeAdded : bool
+            +AddStudent(StudentID)
+         */
+
+        private IUnitOfWork _addStudentRepository;
+        public AddStudentController(IUnitOfWork addStudentRepository)
+        {
+            _addStudentRepository = addStudentRepository;
+        }
+
+        //public IActionResult AddStudent() => View(_addStudentRepository.Students);
+    }
+}
